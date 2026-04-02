b461242bc92c98731d939f0da1f9961e761f6d5f
ishita <dwivediishita60@gmail.com>
2026-04-03 02:52:21 +0530

    updated asset list

--- swatahvision/assets/list.py
+++ swatahvision/assets/list.py
@@ -1,12 +1,22 @@
 class ImageAssets:
     CAR = {
         "filename": "car.jpg",
-        "url": "https://huggingface.co/swatah/swatahvision/resolve/main/images/car.jpg"
+        "url": "https://huggingface.co/datasets/swatah/swatahVision-assets/resolve/main/images/car.jpg"
     }
 
     DOG = {
         "filename": "dog.jpg",
-        "url": "https://huggingface.co/swatah/swatahvision/resolve/main/images/dog.jpg"
+        "url": "https://huggingface.co/datasets/swatah/swatahVision-assets/resolve/main/images/dog.jpg"
+    }
+
+    PEOPLE_WALKING = {
+        "filename": "people-walking.jpg",
+        "url": "https://huggingface.co/datasets/swatah/swatahVision-assets/resolve/main/images/people-walking.jpg"
+    }
+
+    SOCCER = {
+        "filename": "soccer.jpg",
+        "url": "https://huggingface.co/datasets/swatah/swatahVision-assets/resolve/main/images/soccer.jpg"
     }
 
     COVER = {
@@ -25,3 +35,49 @@ class VideoAssets:
         "filename": "traffic.mp4",
         "url": "https://huggingface.co/swatah/swatahvision/resolve/main/videos/vehicles.mp4"
     }
+
+    VEHICLES_2 = {
+        "filename": "vehicles-2.mp4",
+        "url": "https://huggingface.co/datasets/swatah/swatahVision-assets/resolve/main/videos/vehicles-2.mp4"
+    }
+
+    SUBWAY = {
+        "filename": "subway.mp4",
+        "url": "https://huggingface.co/datasets/swatah/swatahVision-assets/resolve/main/videos/subway.mp4"
+    }
+
+    SKIING = {
+        "filename": "skiing.mp4",
+        "url": "https://huggingface.co/datasets/swatah/swatahVision-assets/resolve/main/videos/skiing.mp4"
+    }
+
+    PEOPLE_WALKING = {
+        "filename": "people-walking.mp4",
+        "url": "https://huggingface.co/datasets/swatah/swatahVision-assets/resolve/main/videos/people-walking.mp4"
+    }
+
+    MILK_BOTTLING_PLANT = {
+        "filename": "milk-bottling-plant.mp4",
+        "url": "https://huggingface.co/datasets/swatah/swatahVision-assets/resolve/main/videos/milk-bottling-plant.mp4"
+    }
+
+    MARKET_SQUARE = {
+        "filename": "market-square.mp4",
+        "url": "https://huggingface.co/datasets/swatah/swatahVision-assets/resolve/main/videos/market-square.mp4"
+    }
+
+    GROCERY_STORE = {
+        "filename": "grocery-store.mp4",
+        "url": "https://huggingface.co/datasets/swatah/swatahVision-assets/resolve/main/videos/grocery-store.mp4"
+    }
+
+    BEACH = {
+        "filename": "beach-1.mp4",
+        "url": "https://huggingface.co/datasets/swatah/swatahVision-assets/resolve/main/videos/beach-1.mp4"
+    }
+
+    BASKETBALL = {
+        "filename": "basketball-1.mp4",
+        "url": "https://huggingface.co/datasets/swatah/swatahVision-assets/resolve/main/videos/basketball-1.mp4"
+    }
+
